a82f144eec6d0b68c1e2d891a1b5b0aa663b41ec
Muhammet Berkay Bayrakci <berkay7554@gmail.com>
2023-01-01 18:56:58 +0300

    BMICalculator

---
+++ src/VucutKitleEndeksi/BMICalculator.java
@@ -0,0 +1,23 @@
+package VucutKitleEndeksi;
+
+import java.util.Scanner;
+
+public class BMICalculator {
+    public static void main(String[] args) {
+
+        double kg,m,index;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Kilonuzu giriniz(kg): ");
+        kg = scanner.nextDouble();
+
+        System.out.println("Boyunuzu giriniz(m): ");
+        m = scanner.nextDouble();
+
+        index = kg / m * m;
+        System.out.println("Vücut Kitle İndeksiniz: " + index);
+
+
+    }
+}
