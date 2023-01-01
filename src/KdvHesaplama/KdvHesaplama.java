dc634bea609b5999d0089a8a89e4a81047fe7c3b
Muhammet Berkay Bayrakci <berkay7554@gmail.com>
2023-01-01 17:58:55 +0300

    KDV Hesaplama

---
+++ src/KdvHesaplama/KdvHesaplama.java
@@ -0,0 +1,28 @@
+package KdvHesaplama;
+
+import java.util.Scanner;
+
+public class KdvHesaplama {
+    public static void main(String[] args) {
+
+        double tutar, kdv,kdvTutarı;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ürün Tutarını girin: ");
+        tutar = scanner.nextDouble();
+
+        if(tutar>=0 && tutar<=1000) {
+            kdv=0.18;
+            kdvTutarı = tutar * kdv;
+            System.out.println("KDV tutarı: " + kdvTutarı);
+        }
+        else if(tutar > 1000) {
+            kdv = 0.08;
+            kdvTutarı = tutar * kdv;
+            System.out.println("KDV tutarı: " + kdvTutarı);
+        }
+
+    }
+
+}
