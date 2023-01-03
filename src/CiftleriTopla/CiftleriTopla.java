8d12998f6ca1407a28ad688b4f46d433f825dd43
Muhammet Berkay Bayrakci <berkay7554@gmail.com>
2023-01-03 12:25:06 +0300

    CiftleriTopla

---
+++ src/CiftleriTopla/CiftleriTopla.java
@@ -0,0 +1,23 @@
+package CiftleriTopla;
+
+import java.util.Scanner;
+
+public class CiftleriTopla {
+    public static void main(String[] args) {
+
+        int n,total=0;
+
+        Scanner scanner=new Scanner(System.in);
+
+        do{
+            System.out.println("Sayı giriniz: ");
+            n=scanner.nextInt();
+            if(n%2==0 && n%4==0){
+                total += n;
+            }
+        }while (n%2==0);
+
+        System.out.println("Toplam: " + total);
+
+    }
+}
