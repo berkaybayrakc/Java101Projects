f4f1d37ff5179841a21cea3c7dd2ee77ba5e3f9f
Muhammet Berkay Bayrakci <berkay7554@gmail.com>
2023-01-07 12:47:21 +0300

    Palindrom

---
+++ src/Palindrom/Palindrom.java
@@ -0,0 +1,31 @@
+package Palindrom;
+
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
+public class Palindrom {
+    static boolean isPalindrom(int number){
+        int temp = number,reverseNumber=0,lastNumber;
+        while (temp !=0){
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber*10) + lastNumber;
+            temp = temp /10;
+        }
+
+        return number == reverseNumber ? true:false;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int number = scanner.nextInt();
+
+        if(isPalindrom(number)){
+            System.out.println(number + " Palindrom bir sayıdır.");
+        }
+        else {
+            System.out.println(number + " Palindrom bir sayı değildir.");
+        }
+    }
+}
